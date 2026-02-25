25efa6b1d940337dc2a7bc7d41a6a198c9ad3000
Troll2733 <158507382+Troll2733@users.noreply.github.com>
2026-02-25 11:24:00 +0300

    Update cohort_session_length.sql

--- sql/cohort_session_length.sql
+++ sql/cohort_session_length.sql
@@ -1,1 +1,14 @@
-cohort_session_length.sql
+-- Анализ длительности сессий во время маркетинговой акции
+-- Сравниваем когорту пользователей, пришедших в период акции (ноябрь-декабрь 2022), с остальными
+
+SELECT 
+    CASE 
+        WHEN DATE_TRUNC('month', reg_date) BETWEEN '2022-11-01' AND '2022-12-31' 
+        THEN 'cohort_two_month'
+        ELSE 'other_cohort' 
+    END AS cohort,
+    AVG(end_session - start_session) AS avg_session_length
+FROM skygame.users t1
+LEFT JOIN skygame.game_sessions t2 ON t1.id_user = t2.id_user
+WHERE end_session - start_session > INTERVAL '5 minute'  -- исключаем слишком короткие сессии
+GROUP BY cohort
