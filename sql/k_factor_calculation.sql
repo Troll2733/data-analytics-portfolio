8ac2c82b22005167cad4bc9c4c1ad9c66543ee2e
Troll2733 <158507382+Troll2733@users.noreply.github.com>
2026-02-25 11:36:56 +0300

    Update k_factor_calculation.sql

--- sql/k_factor_calculation.sql
+++ sql/k_factor_calculation.sql
@@ -1,1 +1,10 @@
+-- Расчёт K-factor и прогноз размера будущей когорты
+-- K-factor = сумма приглашений / количество уникальных пользователей
+-- Future cohort = сумма приглашений / количество месяцев
 
+SELECT 
+      SUM(ref_reg) / COUNT(DISTINCT t1.id_user) AS k_factor
+    , SUM(ref_reg) / COUNT(DISTINCT DATE_TRUNC('month', reg_date)) AS future_cohort_size
+FROM skygame.users t1
+LEFT JOIN skygame.referral t2
+       ON t1.id_user = t2.id_user
